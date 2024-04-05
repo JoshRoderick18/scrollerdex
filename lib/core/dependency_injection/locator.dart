46e3862295bf631ebc97a2b6dbb64354e580fc31
joshroderick18 <joshroderick18@gmail.com>
2024-04-04 23:15:43 -0400

    Add SharedPreferencesService to dependency injection

--- lib/core/dependency_injection/locator.dart
+++ lib/core/dependency_injection/locator.dart
@@ -1,12 +1,17 @@
 import 'package:get_it/get_it.dart';
 import 'package:scrollerdex/core/services/http_service.dart';
+import 'package:scrollerdex/core/services/shared_preferences_service.dart';
 import 'package:scrollerdex/data/repositories/pokemon_repository_implementation.dart';
 import 'package:scrollerdex/domain/repositories/pokemon_repository.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 GetIt locator = GetIt.instance;
 
-void setupLocator() {
+void setupLocator() async {
+  locator.registerFactory<SharedPreferencesService>(
+    () => SharedPreferencesService(),
+  );
+
   locator.registerLazySingleton<SnackbarService>(
     () => SnackbarService(),
   );
