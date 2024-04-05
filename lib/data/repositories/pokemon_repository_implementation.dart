087e8a32e91aed3ac111e20e740d2a8b0ce9c129
joshroderick18 <joshroderick18@gmail.com>
2024-04-05 19:03:38 -0400

    Refactor PokemonRepositoryImplementation to use SharedPreferencesService for saving and removing pokemons

--- lib/data/repositories/pokemon_repository_implementation.dart
+++ lib/data/repositories/pokemon_repository_implementation.dart
@@ -1,25 +1,32 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:scrollerdex/core/services/http_service.dart';
+import 'package:scrollerdex/core/services/shared_preferences_service.dart';
 import 'package:scrollerdex/domain/models/pokemon_data_model.dart';
 import 'package:scrollerdex/domain/models/pokemon_model.dart';
 import 'package:scrollerdex/domain/repositories/pokemon_repository.dart';
 
 class PokemonRepositoryImplementation implements PokemonRepository {
   final HttpService _httpService;
+  final SharedPreferencesService _sharedPreferencesService;
 
-  PokemonRepositoryImplementation({required HttpService httpService})
-      : _httpService = httpService {
+  PokemonRepositoryImplementation(
+      {required HttpService httpService,
+      required SharedPreferencesService sharedPreferencesService})
+      : _httpService = httpService,
+        _sharedPreferencesService = sharedPreferencesService {
     _pokemonsData = [];
   }
 
   late List<PokemonData?> _pokemonsData;
+  late List<PokemonData?> _savedPokemons;
 
   @override
   List<PokemonData?> get pokemonsData => _pokemonsData;
 
   @override
-  List<PokemonData?> get savedPokemons => _pokemonsData;
+  List<PokemonData?> get savedPokemons => _savedPokemons;
 
   Future<List<Pokemon>> _getPokemons() async {
     try {
@@ -60,5 +67,30 @@ class PokemonRepositoryImplementation implements PokemonRepository {
     }
 
     throw Exception('Failed to get pokemon data for all pokemons');
+  }
+
+  @override
+  Future<List<PokemonData>> getSavedPokemons() async {
+    final savedPokemonsString =
+        await _sharedPreferencesService.getSavedPokemons();
+    final savedPokemonsJson = savedPokemonsString.map((e) => jsonDecode(e));
+    return savedPokemonsJson.map((e) => PokemonData.fromLocalJson(e)).toList();
+  }
+
+  @override
+  void removePokemon(int index) async {
+    final savedPokemons = await _sharedPreferencesService.getSavedPokemons();
+    savedPokemons.removeAt(index);
+    await _sharedPreferencesService.setSavedPokemons(savedPokemons);
+  }
+
+  @override
+  void savePokemon(PokemonData pokemon) async {
+    final pokemonJson = pokemon.toJson();
+    final pokemonString = jsonEncode(pokemonJson);
+    final savedPokemons = await _sharedPreferencesService.getSavedPokemons();
+    log(name: 'savedPokemons', savedPokemons.toString());
+    savedPokemons.add(pokemonString);
+    await _sharedPreferencesService.setSavedPokemons(savedPokemons);
   }
 }
--- lib/domain/repositories/pokemon_repository.dart
+++ lib/domain/repositories/pokemon_repository.dart
@@ -4,4 +4,7 @@ abstract class PokemonRepository {
   List<PokemonData?> get pokemonsData;
   Future<List<PokemonData>> getPokemonsData();
   List<PokemonData?> get savedPokemons;
+  void savePokemon(PokemonData pokemon);
+  void removePokemon(int index);
+  Future<List<PokemonData?>> getSavedPokemons();
 }
