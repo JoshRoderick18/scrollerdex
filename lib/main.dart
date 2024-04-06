79a5d6f20ec7897518ebc7637c3bbbe3e752bfce
joshroderick18 <joshroderick18@gmail.com>
2024-04-06 15:01:35 -0400

    Add Flutter Native Splash package and configure launch screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:scrollerdex/core/dependency_injection/locator.dart';
 import 'package:scrollerdex/presentation/views/scrollerdex_view.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   setupLocator();
 
   runApp(
     const ProviderScope(
       child: MainApp(),
     ),
+  );
+  await Future.delayed(
+    const Duration(seconds: 5),
+    () {
+      FlutterNativeSplash.remove();
+    },
   );
 }
 
